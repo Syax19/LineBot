a6757e10dd75e73fabdadc9d9748b404f81991d8
Syax19 <nisrules007@gmail.com>
2022-08-31 17:49:59 +0800

    add template

--- app.py
+++ app.py
@@ -208,33 +208,57 @@ message = {
 
 
 def getCarouselMessage(data):
-    message = dict()
+    message = {
+      "type": "template",
+      "altText": "this is a image carousel template",
+      "template": {
+          "type": "image_carousel",
+          "columns": [
+              {
+                "imageUrl": F"{end_point}/static/taipei_101.jpeg",
+                "action": {
+                  "type": "postback",
+                  "label": "台北101",
+                  "data": json.dumps(data)
+                }
+              },
+              {
+                "imageUrl": F"{end_point}/static/taipei_1.jpeg",
+                "action": {
+                  "type": "postback",
+                  "label": "台北101",
+                  "data": json.dumps(data)
+                }
+              }
+          ]
+          }
+        }
     return message
 
 
 def getLocationConfirmMessage(title, latitude, longitude):
-    data = {'title': title, 'latitude': latitude, 'longtitude': longitude, 
+    data = {'title': title, 'latitude': latitude, 'longitude': longitude,
             'action': 'get_near'}
     message = {
-            "type": "template",
-            "altText": "this is a confirm template",
-            "template": {
-                "type": "confirm",
-                "text": f"請確認是否搜尋 {title} 附近地點?",
-                "actions": [                 #  可搜尋Line的Action objects文件
-                    {
-                        "type": "postback",
-                        "label": "是",
-                        "text": json.dumps(data)
-                    },
-                    {
-                        "type": "message",
-                        "label": "否",
-                        "text": "否"
-                    }
-                    ]
-                }
-                }
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": f"確認是否搜尋 {title} 附近地點？",
+          "actions": [
+              {
+                 "type": "postback",
+               "label": "是",
+               "data": json.dumps(data),
+               },
+              {
+                "type": "message",
+                "label": "否",
+                "text": "否"
+              }
+          ]
+      }
+    }
     return message
 
 
